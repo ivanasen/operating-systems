cc2efec505c6000da5c7c4afb373e2fecaa6504a
ivanasen <ivanasenchakarov@gmail.com>
2020-08-21 21:12:46 +0300

    Finish exercise-9/part-1

--- exercise-9/part-1/problem-3.sh
+++ exercise-9/part-1/problem-3.sh
@@ -9,8 +9,22 @@
 # е забранил изпращането на съобщения настройката му да се промени,
 # за да приключи процедурата и след това да се върне забраната.
 
+# This must be run as root in order to be able to enable messages for other users.
+
 file=$1
 shift 1
 users=$*
 
-# TODO: Finish this problem later
+logged_in=$(who | cut -d ' ' -f 1)
+msg=$(cat $file)
+
+for user in $users; do
+    (echo "$logged_in" | grep "$user" &>/dev/null) || continue
+    
+    # write sends messages to the most recent terminal by default
+    (echo "$msg" | write $user) ||
+        (runuser -l $user -c "mesg y" &&
+            echo "$msg" | write $user &&
+            runuser -l $user -c "mesg n")
+done
+
